6b772f4662d9a8039d467b967da51e2041abe15c
ridoy.hossain <ridoy.hossain@bracits.com>
2024-07-04 16:31:26 +0600

    showing date dynamically

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,75 +1,26 @@
-// user-list.component.ts
-
 import { Component, OnInit } from '@angular/core';
-
 import { Member } from '../Member';
-import {Item} from "../Item";
-import {BalanceService} from "../balance/balance.service";
-import {HttpClient} from "@angular/common/http";
-import {ListService} from "./list.service"; // Import the User interface or model
+import { Item } from '../Item';
+import { BalanceService } from '../balance/balance.service';
+import { HttpClient } from '@angular/common/http';
+import { ListService } from './list.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-
 export class UserListComponent implements OnInit {
-  checked:boolean=false;
+  checked: boolean = false;
   membersPin: Member[] = [];
-  items: Item[] = [
-    { date: 'May 1', description: '', amount: 0.0, flag1: false,  },
-    { date: 'May 2', description: '', amount: 0.0, flag1: true,},
-    { date: 'May 3', description: '', amount: 0.0, flag1: false,  },
-    { date: 'May 4', description: '', amount: 0.0, flag1: false,},
-    { date: 'May 5', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 6', description: '', amount: 0.0, flag1: true,},
-    { date: 'May 7', description: '', amount: 0.0, flag1: false, },
-    { date: 'May 8', description: '', amount: 0.0, flag1: false,  },
-    { date: 'May 9', description: '', amount: 0.0, flag1: true,},
-    { date: 'May 10', description: '', amount: 0.0, flag1: false, },
-    { date: 'May 11', description: '', amount: 0.0, flag1: false,  },
-    { date: 'May 12', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 13', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 14', description: '', amount:0.0, flag1: false,  },
-    { date: 'May 15', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 16', description: '', amount: 0.0, flag1: true,  },
-    { date: 'May 17', description: '', amount: 0.0, flag1: false,  },
-    { date: 'May 18', description: '', amount: 0.0, flag1: false,  },
-    { date: 'May 19', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 20', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 21', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 22', description: '', amount: 0.0, flag1: false, },
-    { date: 'May 23', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 24', description: '', amount: 0.0, flag1: false, },
-    { date: 'May 25', description: '', amount: 0.0, flag1: false,  },
-    { date: 'May 26', description: '', amount: 0.0, flag1: true,  },
-    { date: 'May 27', description: '', amount: 0.0, flag1: true,},
-    { date: 'May 28', description: '', amount: 0.0, flag1: true, },
-    { date: 'May 29', description: '', amount: 0, flag1: true, },
-    { date: 'May 30', description: '', amount: 0.0, flag1: false, },
-    { date: 'May 31', description: '', amount: 0.0, flag1: false,  }
-  ];
-
-  daysInMonth: string[] = [
-    'May 1', 'May 2', 'May 3', 'May 4', 'May 5', 'May 6', 'May 7', 'May 8',
-    'May 9', 'May 10', 'May 11', 'May 12', 'May 13', 'May 14', 'May 15',
-    'May 16', 'May 17', 'May 18', 'May 19', 'May 20', 'May 21', 'May 22',
-    'May 23', 'May 24', 'May 25', 'May 26', 'May 27', 'May 28', 'May 29',
-    'May 30', 'May 31'
-  ];
-
+  items: Item[] = [];
   displayedDates: string[] = [];
   displayedItems: Item[] = [];
-  currentStartIndex: number = 0;
+  currentStartDate: Date = new Date();
   itemsPerPage: number = 7;
-  userCheckboxes: { [pin: string]: { [date: string]: boolean } } = {}; // Populate with your data
-  isPreviousDisabled: boolean = true;
-  isNextDisabled: boolean = false;
+  userCheckboxes: { [pin: string]: { [date: string]: boolean } } = {};
 
-
-
-  constructor(private balanceService: BalanceService,private http: HttpClient,private listService:ListService) { }
+  constructor(private balanceService: BalanceService, private http: HttpClient, private listService: ListService) { }
 
   ngOnInit(): void {
     this.loadUsers();
@@ -86,7 +37,6 @@ export class UserListComponent implements OnInit {
       },
       (error) => {
         console.error('Error fetching users:', error);
-        // Handle error, e.g., show error message
       }
     );
   }
@@ -94,38 +44,43 @@ export class UserListComponent implements OnInit {
   initializeCheckboxes(): void {
     this.membersPin.forEach(pin => {
       this.userCheckboxes[pin.pin] = {};
-      this.daysInMonth.forEach(day => {
+      this.displayedDates.forEach(day => {
         this.userCheckboxes[pin.pin][day] = false;
       });
     });
   }
 
   updateDisplayedData() {
-    this.displayedDates = this.daysInMonth.slice(this.currentStartIndex, this.currentStartIndex + this.itemsPerPage);
-    this.displayedItems = this.items.slice(this.currentStartIndex, this.currentStartIndex + this.itemsPerPage);
-    this.isPreviousDisabled = this.currentStartIndex === 0;
-    this.isNextDisabled = this.currentStartIndex + this.itemsPerPage >= this.daysInMonth.length;
+    const endDate = new Date(this.currentStartDate);
+    endDate.setDate(endDate.getDate() + this.itemsPerPage - 1);
+    this.displayedDates = this.generateDateRange(this.currentStartDate, endDate);
+  }
+
+  generateDateRange(startDate: Date, endDate: Date): string[] {
+    const dateArray = [];
+    let currentDate = new Date(startDate);
+    while (currentDate <= endDate) {
+      dateArray.push(currentDate.toDateString());
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return dateArray;
   }
 
   showPrevious() {
-    if (this.currentStartIndex - this.itemsPerPage >= 0) {
-      this.currentStartIndex -= this.itemsPerPage;
-      this.updateDisplayedData();
-    }
+    this.currentStartDate.setDate(this.currentStartDate.getDate() - this.itemsPerPage);
+    this.updateDisplayedData();
   }
 
   showNext() {
-    if (this.currentStartIndex + this.itemsPerPage < this.daysInMonth.length) {
-      this.currentStartIndex += this.itemsPerPage;
-      this.updateDisplayedData();
-    }
+    this.currentStartDate.setDate(this.currentStartDate.getDate() + this.itemsPerPage);
+    this.updateDisplayedData();
   }
 
   checkBoxChange(pin: string, date: string) {
     const selected = this.userCheckboxes[pin][date];
     const memberSelection = { pin, date, selected };
 
-    this.http.post('http://localhost:8080/api/member-selections', memberSelection)
+    this.listService.getMemberSelections()
       .subscribe(response => {
         console.log('Selection saved:', response);
       }, error => {
@@ -135,13 +90,14 @@ export class UserListComponent implements OnInit {
 
   loadFoodItems() {
     this.listService.getFoodItems().subscribe(items => {
-      this.displayedItems = items;
+      this.items = items;
+      this.updateDisplayedData();
     });
   }
 
   loadMemberSelections() {
     this.listService.getMemberSelections().subscribe(selections => {
-      selections.forEach((selection: { pin: string | number; date: string | number; selected: boolean; }) => {
+      selections.forEach((selection: { pin: string; date: string; selected: boolean; }) => {
         if (!this.userCheckboxes[selection.pin]) {
           this.userCheckboxes[selection.pin] = {};
         }
